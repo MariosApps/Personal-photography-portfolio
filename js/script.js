fb75ab7f8a15770bda06ecd16d53fc328d6d30f6
PampuchSkrytozerca <35340456+PampuchSkrytozerca@users.noreply.github.com>
2018-07-19 15:15:00 +0200

    Added gallery

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
     
-  $('.dropdown-submenu a.test').mouseover(function(e){
+  $('.dropdown-submenu a.test').mouseenter(function(e){
     $(this).next('ul').toggle();
     e.stopPropagation();
-  });
-    
-    
-    $('.dropdown-submenu a.test').mouseout(function(e){
-    $(this).next('ul').toggle();
     e.preventDefault();
   });
     
+    
+//    $('.dropdown-submenu a.test').mouseout(function(e){
+//    $(this).next('ul').toggle();
+//    e.preventDefault();
+//  });
+//    
 });
