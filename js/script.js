1a10742d0d698c9983d80de7eb496e2dc0cf0a08
PampuchSkrytozerca <35340456+PampuchSkrytozerca@users.noreply.github.com>
2018-07-20 02:21:03 +0200

    Fixed problem with dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+
+    $(".dropdown-submenu").hover(function () {
+        $(this).children('ul.sub').slideDown('fast');
+    }, function () {
+        $(this).children('ul.sub').slideUp('fast');
+    });
     
-  $('.dropdown-submenu a.test').mouseenter(function(e){
-    $(this).next('ul').toggle();
-    e.stopPropagation();
-    e.preventDefault();
-  });
     
     
-//    $('.dropdown-submenu a.test').mouseout(function(e){
-//    $(this).next('ul').toggle();
-//    e.preventDefault();
-//  });
-//    
+    $(".dropdown").hover(function () {
+        $(this).children('.dropdown-menu').slideDown('fast');
+    }, function () {
+        $(this).children('.dropdown-menu').slideUp('fast');
+    });
+
+
 });
